f749ba6869923b8d987a67db07ee649ee0ee9ab5
mickey <miaoqi@bytedance.com>
2023-02-07 15:57:30 +0800

    chore: temp commit

--- examples/study/line/Meshline.js
+++ examples/study/line/Meshline.js
@@ -4,9 +4,12 @@ export class Meshline extends THREE.BufferGeometry  {
     pointTotal = 0;
     points = [];
     positions = [];
+    previous = [];
+    next = [];
     width = [];
     uvs = [];
-
+    indexes = [];
+    side = [];
 
     constructor(parameters) {
         super(parameters);
@@ -16,25 +19,55 @@ export class Meshline extends THREE.BufferGeometry  {
         this.points = points;
         this.pointTotal = points.length;
 
-        // for (let i = 0; i < points.length; i ++) {
-        //     const p = points[i];
-        //     this.positions.push(p.x, p.y, p.z);
-        //     this.positions.push(p.x, p.y, p.z);
-        // }
-        this.positions.push(0);
-        this.positions.push(1);
-        this.positions.push(0);
+        const start = points[0];
 
-        this.positions.push(-1);
-        this.positions.push(-1);
-        this.positions.push(0);
+        this.previous.push(start.x, start.y, start.z);
+        this.previous.push(start.x, start.y, start.z);
 
-        this.positions.push(1);
-        this.positions.push(-1);
-        this.positions.push(0);
+        for (let i = 0; i < this.pointTotal; i ++) {
+            const p = points[i];
+            this.positions.push(p.x, p.y, p.z);
+            this.positions.push(p.x, p.y, p.z);
+            // this.positions.push(p.x, p.y, p.z);
+
+            this.previous.push(p.x, p.y, p.z);
+            this.previous.push(p.x, p.y, p.z);
+
+            if (i > 0) {
+              this.next.push(p.x, p.y, p.z);
+              this.next.push(p.x, p.y, p.z);
+            }
+
+            // debug
+            // this.positions.push(p.x - 1, p.y - 1, p.z);
+            // this.positions.push(p.x + 1, p.y - 1, p.z);
+            this.side.push(1, -1);
+
+            const index = i * 2;
+
+            if (i < this.pointTotal - 1) {
+                this.indexes.push(index, index + 1, index + 2);
+                // this.indexes.push(index + 2, index + 1, index + 3);
+            }
+        }
+
+        const end = points[this.pointTotal - 1];
+        this.next.push(end.x, end.y, end.z);
+        this.next.push(end.x, end.y, end.z);
 
 
+        for (let i = 0; i < this.pointTotal; i ++) {
+            
+            // this.indexes.push(index, index + 1, index + 2);
+            // this.indexes.push(index, index + 1, index + 2);
+        }
+
+        console.log(this.positions, this.indexes)
         this.setAttribute('position', new THREE.Float32BufferAttribute(this.positions, 3));
+        // this.setAttribute('next', new THREE.Float32BufferAttribute(this.next, 3));
+        // this.setAttribute('previous', new THREE.Float32BufferAttribute(this.previous, 3));
+
+        this.setIndex(new THREE.Uint16BufferAttribute(this.indexes, 1));
         // this.setAttribute('uv', new Float32Array(this.uvs));
         // this.setAttribute('width', new Float32Array(this.width));
         // this.setAttribute('side', new Float32Array(this.width));
@@ -48,6 +81,8 @@ const vert = `
     // attribute vec3 previous;';
     // attribute vec3 next;';
 
+    attribute float next;
+    attribute float previous;
     attribute float side;
     attribute float width;
 
@@ -57,7 +92,9 @@ const vert = `
     void main() {
         vColor = vec4(0., 1., 1., 0.);
 
-        gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        mat4 mvpMatrix = projectionMatrix * modelViewMatrix;
+        gl_Position = mvpMatrix * vec4( position, 1.0 );
+        // vec4 prevPos = mvpMatrix * vec4( ) ;
     }
 `;
 
